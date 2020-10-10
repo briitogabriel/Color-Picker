651c9e0a10bb86f17b8f82727f8eb781c51302bb
Gabriel Brito <gabriel.brito.a@hotmail.com>
2020-10-09 23:00:47 -0300

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
 onload = function() {
     const Range = document.querySelectorAll("input[type='range']");
-    const blocoColor = document.querySelector(".Cor");
+    const blocoColor = document.querySelector(".color");
     const value = document.querySelectorAll(".value");
-    const cod = document.querySelector('p');
+    const cod = document.querySelector('strong');
 
     for(i=0; i < Range.length ; i++){
 
-        Range[i].addEventListener('input', ()=>{
+        Range[i].addEventListener('input', () => {
 
             let Red = Range[0].value;
             let Green = Range[1].value;    
@@ -22,37 +22,3 @@ onload = function() {
         })
     }
 };
-
-// window.addEventListener('load',start);
-
-// function start(){
-//     rangeRed = document.querySelector('#red');
-//     rangeGreen = document.querySelector('#green');
-//     rangeBlue = document.querySelector('#blue');
-
-//     inputRed = document.querySelector('#inputRed');
-//     inputGreen = document.querySelector('#inputGreen');
-//     inputBlue = document.querySelector('#inputBlue');
-
-//     divColor = document.querySelector('#color');
-
-//     rangeRed = addEventListener('input', setColor);
-//     rangeGreen = addEventListener('input', setColor);
-//     rangeBlue = addEventListener('input', setColor);
-
-//     setColor();
-// };
-
-// function setColor(){
-//     var red = parseInt(rangeRed.value, 10); //converte para números decimais = 10
-//     var green = +rangeGreen.value;
-//     var blue = +rangeBlue.value;
-
-//     inputRed.value = red;
-//     inputGreen.value = green;
-//     inputBlue.value = blue;
-
-//     var rgbCSS = 'rgb(' + red + ',' + green + ',' + blue + ')';
-
-//     divColor.style.backgroundColor = rgbCSS;
-// };
